1a5d4b086bc678d54081fa3c572964d588e6dbeb
chih-hsi-chen <chih.hsi.chen@gmail.com>
2020-04-10 10:27:19 +0800

    Updates

--- precache-manifest.b37f543a83a7586d6310577db727bfa4.js
+++ precache-manifest.b37f543a83a7586d6310577db727bfa4.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "bcca948dec9218e26de85547bed2e6df",
+    "revision": "a851630cbabbb7516c6ba4bc463b51a6",
     "url": "/ExShop/index.html"
   },
   {
-    "revision": "b8dd04ff082319178eb9",
-    "url": "/ExShop/static/css/main.34af7c7d.chunk.css"
+    "revision": "27792a8f63581f1cd7d7",
+    "url": "/ExShop/static/css/main.db7fbf22.chunk.css"
   },
   {
     "revision": "561dca1810b5b152093c",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/ExShop/static/js/2.de964c88.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b8dd04ff082319178eb9",
+    "revision": "27792a8f63581f1cd7d7",
     "url": "/ExShop/static/js/main.3463da67.chunk.js"
   },
   {
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ExShop/precache-manifest.4a300ca5f695dff9602672eef31b23d5.js"
+  "/ExShop/precache-manifest.b37f543a83a7586d6310577db727bfa4.js"
 );
 
 self.addEventListener('message', (event) => {
